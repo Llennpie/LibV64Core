172266c99bb4ae8331638c5431dbd514dc27d496
Llennpie <44985633+Llennpie@users.noreply.github.com>
2021-08-31 16:39:49 -0400

    Add base

---
+++ LibV64Core/Core.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibV64Core
+{
+    public class Core
+    {
+        #region Camera
+        public static bool CameraFrozen;
+
+        /// <summary>
+        /// Freezes/unfreezes the game camera. Defaults to true.
+        /// </summary>
+        /// <param name="freeze"></param>
+        public static void FreezeCamera(bool freeze = true)
+        {
+            if (!Memory.IsEmulatorOpen || Memory.BaseAddress == 0)
+                return;
+
+            if (freeze) {
+                byte[] writeData = { 0x80 };
+                Memory.WriteBytes(Memory.BaseAddress + 0x33C84B, writeData);
+                CameraFrozen = true;
+            }
+            else
+            {
+                byte[] writeData = { 0x00 };
+                Memory.WriteBytes(Memory.BaseAddress + 0x33C84B, writeData);
+                CameraFrozen = false;
+            }
+        }
+
+        /// <summary>
+        /// Zero-fills the camera zoom out flags, allowing the camera to be frozen in any level.
+        /// </summary>
+        public static void FixCameraZoomOut()
+        {
+            if (!Memory.IsEmulatorOpen || Memory.BaseAddress == 0)
+                return;
+
+            Memory.WriteBytes(Memory.BaseAddress + 0x32F870, new byte[20]);
+        }
+        #endregion
+
+        /// <summary>
+        /// Reloads all core values (such as CameraFrozen) from their in-game addresses. Useful when the tool relaunches after values were modified.
+        /// </summary>
+        public static void ReloadPreviousValues()
+        {
+            byte[] freezeCameraData = Memory.SwapEndian(Memory.ReadBytes(Memory.BaseAddress + 0x33C84B, 1), 4);
+
+            if (freezeCameraData[0] == 0x80)
+                CameraFrozen = true;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# v64core
+# LibV64Core
+
+- **LibV64Core** is an experimental C# class library aiming to assist developers with creating *Super Mario 64* machinima tools.
+- This library was designed to be as simple and lightweight as possible, while keeping core features intact.
+
+## Usage
+
+First, add **LibV64Core** to your existing tool's project. In **Visual Studio 2019**:
+- Right click your Solution in the Solution Explorer and navigate to *Add -> Existing Project...*
+- Select `LibV64Core.csproj`.
+- Right click your tool's project in the Solution Explorer and navigate to *Add -> Reference...*
+- Under *Project -> Solution* select **LibV64Core**.
+
+**LibV64Core** should now be ready for use. Here's a quick example in **Windows Forms**:
+
+```csharp
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using LibV64Core;
+
+namespace SampleToolGui
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Process[] emulatorProcesses = Memory.GetEmulatorProcesses("Project64");
+            Memory.HookEmulatorProcess(emulatorProcesses[0]);
+            Memory.FindBaseAddress();
+            Core.ReloadPreviousValues();
+
+            if (Core.CameraFrozen)
+                Debug.WriteLine("Camera was already frozen!");
+
+            Core.FixCameraZoomOut();
+        }
+    }
+}
+```
+
+Documentation is coming soon.
+
+## Credits
+
+- *[Project Comet](https://github.com/projectcomet64)* and *[GlitchyPSI](https://github.com/GlitchyPSIX)* for creating [M64MM3](https://github.com/projectcomet64/M64MM).
+- *[James "CaptainSwag101" Pelster](https://github.com/jpmac26)* for creating [M64MM2](https://github.com/jpmac26/M64MM2).
